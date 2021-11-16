5b57989b057f6288bc8f5bfc6f22430a3eafae6a
todd <andersontodds@gmail.com>
2021-11-15 18:00:50 -0800

    getpaths.jl appears to be working, need to specify file output type

--- getpaths.jl
+++ getpaths.jl
@@ -2,6 +2,7 @@
 
 using MAT
 using Printf
+using BenchmarkTools
 
 function getpaths(startdate; stopdate=startdate, frames=144)
 
@@ -9,6 +10,9 @@ function getpaths(startdate; stopdate=startdate, frames=144)
     statfile = matopen("stations.mat")
     stations = read(statfile,"stations")
     close(statfile)
+    # add line numbers to stationlist
+    stnum = 1:1:size(stations,1)
+    stations = cat(stationlist[:,1:3], stnum; dims=2)
 
     APdata = zeros(0,10)        # APfile.data has dimension N x 10
     APpower = zeros(70,0)       # APfile.power has dimension 70 x N
@@ -20,12 +24,12 @@ function getpaths(startdate; stopdate=startdate, frames=144)
         APfilepower = read(APfile,"power")
 
         # concatenate total data, power with current file data, power
-        APdata = [APdata; APfiledata]
-        APpower = [APpower APfilepower]
+        APdata = cat(APdata, APfiledata; dims=1)
+        APpower = cat(APpower, APfilepower; dims=2)
         close(APfile)
     end
 
-    return APpower
+    # return APdata
 
     # convert station ID from 0-start (in stations.mat, stations.dat) to 1-start; except station 0 (Dunedin)
     # since stations are listed in order of ascending stID in each column of APpower[], can add 1 to all station IDs, then find all station IDs not on the first row and set them back to 0.  The fastest alternative that I know of would be to add 1 to all elements of the first row of strows[], then add 1 to all nonzero rows of @view strows[2:end, :].
@@ -38,39 +42,38 @@ function getpaths(startdate; stopdate=startdate, frames=144)
     nullrows = NaN.*ones(size(strows))
     stmat = ones(size(APpower))
     stmat[1:2:end, :] = strows;
-    stmat[2:2:end, :] = nullrows;   # is it necessary to pad stmat with NaNs in order to make its size equal to APpower?
+    stmat[2:2:end, :] = nullrows;   #QUESTION is it necessary to pad stmat with NaNs in order to make its size equal to APpower? → useful if station energy needs to be used later.
 
     # find every stationID; as of 4 Oct 2018 there are 122 entries in stations.dat
     # NOTE: stations.dat and .mat index from stID == 1 (Dunedin), not 0
     pairlist = zeros(0,13)    # "strokelist", in MATLAB version
     for stID = 1:122
-        #NOTE: MATLAB code below.  Need to find the columns in APpower in which stID exists, then find the APdata corresponding to those detections, and thereby compile subset of APdata that includes only strokes detected by current stID.
+        stindex = Tuple.(findall(x -> x==stID, stmat)) # defaults to CartesianIndex without Tuple.()
+        foundrows = first.(stindex)
+        foundcols = last.(stindex)
 
-        stindex = findall(x -> x==stID, stmat)
-        #lidx = sub2ind(size(APpower),row + 1, col)
-        #APdata_stID = flrAPdata(col,:);
-        #APpower_stID = flrAPpower(lidx);
+        APdata_stID = APdata[foundcols,:]
 
-        # return list of stroke time, location for each station
-        # need dimension stID, or vectorize and concatenate
+        st_lat = stations[stID, 1]
+        st_lon = stations[stID, 2]
 
-        #strokecount_stID = size(APdata_stID,1);
-        #c = ones(strokecount_stID,1);
+        strokecount_stID = size(foundcols, 1)
+        strokelist_stID = cat(APdata_stID, stID*ones(strokecount_stID,1), st_lat*ones(strokecount_stID,1), st_lon*ones(strokecount_stID,1); dims=2)
 
-        #st_lat = stations{stID,1};
-        #st_lon = stations{stID,2};
-
-        #strokelist_stID = cat(2,APdata_stID,stID*c,st_lat*c,st_lon*c);
-
-        #strokelist = cat(1,strokelist,strokelist_stID);
+        pairlist = cat(pairlist, strokelist_stID; dims=1)
     end
 
+    return pairlist
 
 end
 
 ## scratch
 
-power = getpaths(20170906)
+using Plots
+
+APdata = getpaths(20170906)
+APpower = power
+
 
 strows = power[1:2:end,:]
 strows = strows .+ 1
@@ -94,7 +97,50 @@ statfile = matopen("stations.mat")
 stations = read(statfile,"stations")
 close(statfile)
 
-
+#stnum = 1:1:size(stations,1)
+#stations = cat(stnum, stationlist[:,1:2]; dims=2)
 
 stID = 11
-stindex = findall(x -> x==stID, stmat) # how to separate rows and columns in vector of CartesianIndex{2}?
+stindex = Tuple.(findall(x -> x==stID, stmat)) # how to separate rows and columns in vector of CartesianIndex{2}?
+foundrows = first.(stindex)
+foundcols = last.(stindex)
+
+APdata_stID = APdata[foundcols,:]
+
+st_lat = stations[stID, 1]
+st_lon = stations[stID, 2]
+
+strokecount_stID = size(foundcols, 1)
+strokelist_stID = cat(APdata, st_lat*ones(strokecount_stID,1), st_lon*ones(strokecount_stID,1); dims=2)
+
+strokelist = cat(strokelist, strokelist_stID; dims=1)
+
+# benchmark for loop
+pairlist = zeros(0,13)    # "strokelist", in MATLAB version
+for stID = 1:122
+    stindex = Tuple.(findall(x -> x==stID, stmat)) # defaults to CartesianIndex without Tuple.()
+    foundrows = first.(stindex)
+    foundcols = last.(stindex)
+
+    APdata_stID = APdata[foundcols,:]
+
+    st_lat = stations[stID, 1]
+    st_lon = stations[stID, 2]
+
+    strokecount_stID = size(foundcols, 1)
+    strokelist_stID = cat(APdata_stID, stID*ones(strokecount_stID,1), st_lat*ones(strokecount_stID,1), st_lon*ones(strokecount_stID,1); dims=2)
+
+    pairlist = cat(pairlist, strokelist_stID; dims=1)
+end
+
+pl_20170906 = getpaths(20170906)
+
+k = 20170906
+APfilename = @sprintf("C:/Users/tshelbya/Documents/MATLAB/Lightning_matlab/APfiles/AP%d.mat", k)
+APfile = matopen(APfilename)
+APfiledata = read(APfile,"data")
+
+in_len = size(APfiledata,1)
+out_len = size(pl_20170906,1)
+
+out_len/in_len
