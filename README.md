985c785685fd027ca3da48284c7102d9d6476132
todd <andersontodds@gmail.com>
2021-11-16 17:02:39 -0800

    get pathlist_lite_10m and file output working

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
 # pathGrid-JL
 
 Julia implementation of the pathGrid MATLAB project.  Until this repository is released, see the pathGrid repository: https://github.com/andersontodds/pathGrid
+
+# TODO
+11-16-2021: get time binning and fileoutput working with getpaths.jl; fill out outline.
+
+# Functions
+getpaths(date; fileout="lite")
+  getpaths takes a date as input (integer of format YYYYMMDD, e.g. 20170906) and outputs a file or set of files containing the times and endpoints of lightning stroke-to-station paths.
+  arguments:
+    date    integer
+
+
+# Outline
+main
+  pathfile():
+    input: date range
+    parameters: fileout
+    calls: getpaths()
+    getpaths()
+      input: date
+      output: stroke-station pair list
+    output: file(s) of stroke-station pairs
+
+or
+
+  getpaths():
+    input: date range
+    parameter: fileout
+    output: file(s) of stroke-station pairs
--- getpaths.jl
+++ getpaths.jl
@@ -3,6 +3,7 @@
 using MAT
 using Printf
 using BenchmarkTools
+using Dates
 
 function getpaths(startdate; stopdate=startdate, frames=144)
 
@@ -17,8 +18,12 @@ function getpaths(startdate; stopdate=startdate, frames=144)
     APdata = zeros(0,10)        # APfile.data has dimension N x 10
     APpower = zeros(70,0)       # APfile.power has dimension 70 x N
 
-    for k = startdate:stopdate
-        APfilename = @sprintf("C:/Users/tshelbya/Documents/MATLAB/Lightning_matlab/APfiles/AP%d.mat", k)
+    datestart = Date(string(startdate), "yyyymmdd")
+    datestop = Date(string(stopdate), "yyyymmdd")
+
+    for k = datestart:Day(1):datestop      # possibility of crossing month boundary in range requires indexing through Date rather than Int, then converting back to something parseable by @sprintf
+        k_str = Dates.format(k, "yyyymmdd")
+        APfilename = @sprintf("C:/Users/tshelbya/Documents/MATLAB/Lightning_matlab/APfiles/AP%s.mat", k_str)
         APfile = matopen(APfilename)
         APfiledata = read(APfile,"data")
         APfilepower = read(APfile,"power")
@@ -61,9 +66,25 @@ function getpaths(startdate; stopdate=startdate, frames=144)
         strokelist_stID = cat(APdata_stID, stID*ones(strokecount_stID,1), st_lat*ones(strokecount_stID,1), st_lon*ones(strokecount_stID,1); dims=2)
 
         pairlist = cat(pairlist, strokelist_stID; dims=1)
+
     end
 
     return pairlist
+
+    # TODO 11/16/2021: get pairlist_lite_10m and file output working.
+    stroke_time = DateTime.(pairlist[:,1:6])
+    stroke_lat = pairlist[:,7]
+    stroke_lon = pairlist[:,8]
+    stat_lat = pairlist[:,12]
+    stat_lon = pairlist[:,13]
+    pairlist_lite = cat(stroke_time, stroke_lat, stroke_lon, stat_lat, stat_lon, dims=2)
+
+    bin_edges_10min = datestart:Minute(10):datestop
+
+    for t = 1:size(bin_edges_10min,1)
+        pairlist_lite_10m =
+
+    end
 
 end
 
@@ -144,3 +165,59 @@ in_len = size(APfiledata,1)
 out_len = size(pl_20170906,1)
 
 out_len/in_len
+
+# DateTime only supports millisecond precision.  Can either
+#   a) round stroke_time to nearest millisecond,
+#   b) split stroke_time into Date (YYYYMMDD) and Time (hh:mm:ss.)
+stroke_date = DateTime.(pairlist[:,1:6])
+stroke_lat = pairlist[:,7]
+stroke_lon = pairlist[:,8]
+stat_lat = pairlist[:,12]
+stat_lon = pairlist[:,13]
+pairlist_lite = cat(stroke_time, stroke_lat, stroke_lon, stat_lat, stat_lon, dims=2)
+
+times = pairlist
+
+size(times, 1)
+
+times = [   2017 09 06 00 00 04.911360;
+            2017 09 06 04 15 55.193727;
+            2017 09 06 22 55 12.256655]
+
+sdate = Date(times[1,1]):Day(1):Date(times[3,1])
+stime = Time(times[1,4]):Second(1):Time(times[3,4])
+
+sdate = Vector{Date}
+
+for i = 1:size(times, 1)
+    #stroke_date = times[1,1:3]
+    years   = times[i,1]
+    months  = times[i,2]
+    days    = times[i,3]
+    hours   = times[i,4]
+    minutes = times[i,5]
+    seconds = floor(times[i,6])
+    millis  = floor((times[i,6] - seconds)*1000)
+    micros  = floor(((times[i,6] - seconds)*1000 - millis)*1000)
+
+    sdate[i] = Date(years, months, days)
+    stime[i] = Time(hours, minutes, seconds, millis, micros)
+
+end
+
+i = 1
+years   = times[i,1]
+months  = times[i,2]
+days    = times[i,3]
+hours   = times[i,4]
+minutes = times[i,5]
+seconds = floor(times[i,6])
+millis  = floor((times[i,6] - seconds)*1000)
+micros  = floor(((times[i,6] - seconds)*1000 - millis)*1000)
+
+sdate = Date(years, months, days)
+stime = Time(hours, minutes, seconds, millis, micros)
+
+date_vector = [[2017 09 06], [2017 09 07], [2017 09 08]]
+
+sdate = Date.(date_vector)
